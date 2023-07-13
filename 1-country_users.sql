ba40bc9b02abe045fd8804cae8bfbad7df194065
Adio Adebayo <zillionaire234@gmail.com>
2023-07-13 18:47:02 +0100

    Update 1-country_users.sql

--- 1-country_users.sql
+++ 1-country_users.sql
@@ -1,8 +1,5 @@
 -- Script to create the users table with country enumeration
 
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
-
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
