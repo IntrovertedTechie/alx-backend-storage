1f8585ccc9ff592a791524028c4b66388d176361
Adio Adebayo <zillionaire234@gmail.com>
2023-07-13 20:53:47 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
---SQL script to list bands with Glam rock 
+-- SQL script to list bands with Glam rock as main style, ranked by longevity
+
 SELECT band_name, (2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
