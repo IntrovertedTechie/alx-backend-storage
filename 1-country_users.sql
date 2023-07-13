d9d367fc727fe23b0104987a42d8f5b79bb76f51
introvertedtechie <zillionaire234@gmail.com>
2023-07-13 16:45:13 +0000

    sql

--- 1-country_users.sql
+++ 1-country_users.sql
@@ -1,7 +1,9 @@
--- Create table users if it doesn't exist
+-- SQL script that creates a country column 
+
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country VARCHAR(2) NOT NULL DEFAULT 'US',
+    CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'))
 );
