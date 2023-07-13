f5db27a902cc07411d63179689a83eddd4c7b703
introvertedtechie <zillionaire234@gmail.com>
2023-07-13 17:44:49 +0000

    adv sql

--- 1-country_users.sql
+++ 1-country_users.sql
@@ -1,9 +1,11 @@
--- SQL script that creates a country column 
+-- Script to create the users table with country enumeration
+
+CREATE DATABASE IF NOT EXISTS holberton;
+USE holberton;
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country VARCHAR(2) NOT NULL DEFAULT 'US',
-    CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'))
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
