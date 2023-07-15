a21566e3c262b49848dada64bf2999d333880507
Adio Adebayo <zillionaire234@gmail.com>
2023-07-15 18:22:31 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,7 +2,7 @@
 
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    -- Calculate the sum of scores and count of corrections for the user
+    
     DECLARE sum_score INT;
     DECLARE count_corrections INT;
     DECLARE avg_score FLOAT;
@@ -11,14 +11,14 @@ BEGIN
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Compute the average score
+   
     IF count_corrections > 0 THEN
         SET avg_score = sum_score / count_corrections;
     ELSE
         SET avg_score = 0;
     END IF;
 
-    -- Update the average_score for the user
+  
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
