7e7d51be844a8f662ffad7d2c9a96e00abbbae57
Introvertedtechie <zillionaire234@gmail.com>
2023-07-19 16:39:44 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -51,43 +51,31 @@ class Cache:
         # Automatically parametrize Cache.get with the correct conversion function
         return self.get(key, fn=int)
 
-def call_history(method: Callable) -> Callable:
+def count_calls(method: Callable) -> Callable:
     # Use functools.wraps to conserve the original function's name and docstring
     @wraps(method)
     def wrapped_method(self, *args, **kwargs):
-        # Create the input and output list keys
-        inputs_key = "{}:inputs".format(method.__qualname__)
-        outputs_key = "{}:outputs".format(method.__qualname__)
+        # Create a unique key for the method using its qualified name
+        key = method.__qualname__
 
-        # Append the input arguments to the input list in Redis
-        self._redis.rpush(inputs_key, str(args))
+        # Increment the call count for the method
+        self._redis.incr(key)
 
-        # Execute the original method to get the output
-        output = method(self, *args, **kwargs)
-
-        # Append the output to the output list in Redis
-        self._redis.rpush(outputs_key, output)
-
-        return output
+        # Execute the original method and return its result
+        return method(self, *args, **kwargs)
 
     return wrapped_method
 
-# Decorate Cache.store with call_history
-Cache.store = call_history(Cache.store)
+# Decorate Cache.store with count_calls
+Cache.store = count_calls(Cache.store)
 
 # Test the implementation
 if __name__ == "__main__":
     cache = Cache()
 
-    s1 = cache.store("first")
-    print(s1)
-    s2 = cache.store("second")
-    print(s2)
-    s3 = cache.store("third")
-    print(s3)
+    cache.store(b"first")
+    print(cache.get(cache.store.__qualname__))  # Output: b'1'
 
-    inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-    outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
-
-    print("inputs: {}".format(inputs))
-    print("outputs: {}".format(outputs))
+    cache.store(b"second")
+    cache.store(b"third")
+    print(cache.get(cache.store.__qualname__))  # Output: b'3'
